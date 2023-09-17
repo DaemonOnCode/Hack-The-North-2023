47aaa6b3b1616df66ab86a6f4ef9e55f7be05887
Puneet Gupta <puneetgupta.dtu@gmail.com>
2023-09-17 04:12:28 -0400

    Setup serverless for lambda deployments
    Signed-off-by: Puneet Gupta <puneetgupta.dtu@gmail.com>

--- ocr/handler.py
+++ ocr/handler.py
@@ -1,24 +1,38 @@
+import boto3
 import json
+import requests
+from pypdf import PdfReader
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def download_pdf(bucketname: str, filename: str):
+    s3 = boto3.client("s3")
+    fileObj = s3.get_object(Bucket=bucketname, Key=filename)
+    file_content = fileObj["Body"].read()
+    return file_content
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def call_webhook(key, text: str):
+    # we'll split the PDF file name to retrieve job application ID
+    application_id = key.split("__")[-1].split(".")[0]
+    requests.post("https://api.onehirehub.tech/v1/jobs/applications/webhook",
+                  json={"text": text, "job_application_id": application_id})
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def read_pdf(file):
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+
+def lambda_handler(event, _):
+    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+
+    pdf_file = download_pdf(
+        bucket_name,
+        key,
+    )
+    text = read_pdf(pdf_file)
+    call_webhook(key, text)
