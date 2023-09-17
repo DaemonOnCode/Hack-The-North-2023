ee7e2f80abe07d8fa652c337d1bfd64f235745b5
Puneet Gupta <puneetgupta.dtu@gmail.com>
2023-09-17 01:10:35 -0400

    Update allowed hosts
    Signed-off-by: Puneet Gupta <puneetgupta.dtu@gmail.com>

--- backend/ats/settings.py
+++ backend/ats/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)7b5rs(vab4!(ar9*(9nv&*b4sd2i^ii-3hk_tk(h-h=-dl)rb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["api.onehirehub.tech"]
+ALLOWED_HOSTS = ["api.onehirehub.tech", "localhost"]
 
 # Application definition
 
